f3cb024a58cea3caece9249c98d28038e0835461
CandanGitup20 <candan1222@gmail.com>
2024-02-21 17:16:52 +0300

    task 3

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -3,14 +3,14 @@ package Gun01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba 1");
-        System.out.println("Merhaba 2");
-        System.out.println("Merhaba 3");
-
-        for (int i = 0; i < ; i++) {
-            System.out.println("Merhaba");
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Merhaba 5");
 
         }
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba");
+        }
 
 
     }
